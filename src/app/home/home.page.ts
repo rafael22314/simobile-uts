1bafa5cd1ead0b88c3f4a609896f32ee7caa86fe
Vuai-Hub <vuai.harianto78@gmail.com>
2026-09-25 21:41:21 +0700

    menambah tampilan ringkasan jumlah produk, total transaksi, produk terlaris

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  jumlahProduk: number = 0;
+  totalTransaksiHariIni: number = 0;
+  produkTerlaris: string = "-";
 
   constructor() {}
 
