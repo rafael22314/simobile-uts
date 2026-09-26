aec179e80694246a214b2b24e511ca4757f240c4
Vuai-Hub <vuai.harianto78@gmail.com>
2026-09-26 11:32:12 +0700

    melakukan pengaturan page untuk menganti tema warna dan menambah dark mode

--- src/app/pengaturan/pengaturan.page.ts
+++ src/app/pengaturan/pengaturan.page.ts
@@ -8,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PengaturanPage implements OnInit {
 
-  constructor() { }
+  darkMode: boolean = false;
+
+  constructor() {}
 
   ngOnInit() {
+    this.darkMode = localStorage.getItem('darkMode') === 'true';
+
+    if (this.darkMode) {
+      document.body.classList.add('dark');
+    }
+  }
+
+  toggleDarkMode(event: any) {
+    this.darkMode = event.detail.checked;
+
+    if (this.darkMode) {
+      document.body.classList.add('dark');
+      localStorage.setItem('darkMode', 'true');
+    } else {
+      document.body.classList.remove('dark');
+      localStorage.setItem('darkMode', 'false');
+    }
   }
 
 }
